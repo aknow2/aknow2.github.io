d08e9254df114992245f846cc86eead29675888a
tmknym <mushimushikera@gmail.com>
2019-01-11 23:20:39 +0900

    add meta

---
+++ _next/static/4Xf~7Lz7TKQ7NQGCabo58/pages/index.js
@@ -0,0 +1,47 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([[4],{
+
+/***/ 547:
+/***/ (function(module, exports, __webpack_require__) {
+
+__NEXT_REGISTER_PAGE('/', function() {
+module.exports = __webpack_require__(548);
+
+return { page: module.exports.default }});
+
+/***/ }),
+
+/***/ 548:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(39);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(217);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(223);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+var index = function index() {
+  return react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"](next_head__WEBPACK_IMPORTED_MODULE_3___default.a, null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("title", null, "aknow2"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("link", {
+    rel: "manifest",
+    href: "/static/manifest.json"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("link", {
+    rel: "shortcut icon",
+    href: "/static/favicon.ico"
+  })), react__WEBPACK_IMPORTED_MODULE_0__["createElement"](_components_App__WEBPACK_IMPORTED_MODULE_1__[/* default */ "b"], {
+    initPos: _components_App__WEBPACK_IMPORTED_MODULE_1__[/* InitPos */ "a"].top
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (index);
+
+/***/ })
+
+},[[547,1,0]]]));;
+//# sourceMappingURL=index.js.map
