5b87051cf04608ab2c96e7ec5daa75641b62e79b
tmknym <mushimushikera@gmail.com>
2019-01-25 21:35:40 +0900

    create contact page

---
+++ _next/static/Udl6Vl5CEwlMdVm0Q5Jah/pages/index.js
@@ -0,0 +1,83 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([[4],{
+
+/***/ 547:
+/***/ (function(module, exports, __webpack_require__) {
+
+__NEXT_REGISTER_PAGE('/', function() {
+module.exports = __webpack_require__(548);
+
+return { page: module.exports.default }});
+
+/***/ }),
+
+/***/ 548:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(39);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(217);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(223);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+var index = function index() {
+  return react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"](next_head__WEBPACK_IMPORTED_MODULE_3___default.a, null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "viewport",
+    content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "theme-color",
+    content: "#000000"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "description",
+    content: "\u4E09\u91CD\u770C\u6D25\u5E02\u306E\u30BD\u30D5\u30C8\u30A6\u30A7\u30A2\u958B\u767A\u8005\u3067\u3059\u3002\u4E3B\u306B\u30D5\u30ED\u30F3\u30C8\u30A8\u30F3\u30C9\u3084\u30E2\u30D0\u30A4\u30EB\u30FB\u30C7\u30B9\u30AF\u30C8\u30C3\u30D7\u30A2\u30D7\u30EA\u958B\u767A\u3092\u5F97\u610F\u3068\u3057\u3066\u3044\u307E\u3059\u3001\u304A\u6C17\u8EFD\u306B\u304A\u554F\u3044\u5408\u308F\u305B\u4E0B\u3055\u3044\u3002"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    property: "og:locale",
+    content: "ja_JP"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    property: "og:type",
+    content: "website"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    property: "og:description",
+    content: "\u4E09\u91CD\u770C\u6D25\u5E02\u306E\u30BD\u30D5\u30C8\u30A6\u30A7\u30A2\u958B\u767A\u8005\u3067\u3059\u3002\u4E3B\u306B\u30D5\u30ED\u30F3\u30C8\u30A8\u30F3\u30C9\u3084\u30E2\u30D0\u30A4\u30EB\u30FB\u30C7\u30B9\u30AF\u30C8\u30C3\u30D7\u30A2\u30D7\u30EA\u958B\u767A\u3092\u5F97\u610F\u3068\u3057\u3066\u3044\u307E\u3059\u3001\u304A\u6C17\u8EFD\u306B\u304A\u554F\u3044\u5408\u308F\u305B\u4E0B\u3055\u3044\u3002"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    property: "og:title",
+    content: "aknow2"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    property: "og:url",
+    content: "https://aknow2.com/"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "twitter:site",
+    content: "@aknow21"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "twitter:creator",
+    content: "@aknow21"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "twitter:domain",
+    content: "aknow2.com"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "twitter:card",
+    content: "summary"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("title", null, "aknow2"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("link", {
+    rel: "manifest",
+    href: "/static/manifest.json"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("link", {
+    rel: "shortcut icon",
+    href: "/static/favicon.ico"
+  })), react__WEBPACK_IMPORTED_MODULE_0__["createElement"](_components_App__WEBPACK_IMPORTED_MODULE_1__[/* default */ "b"], {
+    initPos: _components_App__WEBPACK_IMPORTED_MODULE_1__[/* InitPos */ "a"].top
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (index);
+
+/***/ })
+
+},[[547,1,0]]]));;
+//# sourceMappingURL=index.js.map
