25b326aa1d7a8d3b9d199e8b7a877680c508a3c4
tmknym <mushimushikera@gmail.com>
2019-03-29 10:08:29 +0900

    add hatena icon

---
+++ _next/static/UOlnjKMhibe2owG~HT3VY/pages/prj.js
@@ -0,0 +1,53 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([[6],{
+
+/***/ 554:
+/***/ (function(module, exports, __webpack_require__) {
+
+__NEXT_REGISTER_PAGE('/prj', function() {
+module.exports = __webpack_require__(555);
+
+return { page: module.exports.default }});
+
+/***/ }),
+
+/***/ 555:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_App__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(39);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(217);
+/* harmony import */ var _index_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_index_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(223);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+var index = function index() {
+  return react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("div", null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"](next_head__WEBPACK_IMPORTED_MODULE_3___default.a, null, react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "viewport",
+    content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("meta", {
+    name: "theme-color",
+    content: "#000000"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("title", null, "aknow2"), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("link", {
+    rel: "manifest",
+    href: "/static/manifest.json"
+  }), react__WEBPACK_IMPORTED_MODULE_0__["createElement"]("link", {
+    rel: "shortcut icon",
+    href: "/static/favicon.ico"
+  })), react__WEBPACK_IMPORTED_MODULE_0__["createElement"](_components_App__WEBPACK_IMPORTED_MODULE_1__[/* default */ "b"], {
+    initPos: _components_App__WEBPACK_IMPORTED_MODULE_1__[/* InitPos */ "a"].myPrj
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (index);
+
+/***/ })
+
+},[[554,1,0]]]));;
+//# sourceMappingURL=prj.js.map
